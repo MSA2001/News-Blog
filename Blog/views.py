0f920094ea2dc04439dff3da97c34dbb6c2a4bdf
MSA2001 <msa2001b21@gmail.com>
2022-11-05 19:43:53 +0330

    category template added

--- Blog/views.py
+++ Blog/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'Blog/index-7.html')
+
+def cat_lifestyle(request):
+    return render(request, 'Blog/post-category-3.html')
