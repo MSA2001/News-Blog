4686f53f1083207dcf1b066d2a3fe53e727c7a57
MSA2001 <msa2001b21@gmail.com>
2022-11-07 16:28:20 +0330

    change in models.py

--- Blog/models.py
+++ Blog/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.title
+
+
+class Article(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ManyToManyField(Category, related_name='articles')
+    title = models.CharField(max_length=100)
+    body = models.TextField(max_length=2000)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='profile/image', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+
