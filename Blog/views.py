a2fd3d21794b9ccb1e6f58be21fbbd33aa01ea2e
MSA2001 <msa2001b21@gmail.com>
2022-11-07 17:34:57 +0330

    login/register created & added (Demo)

--- Blog/views.py
+++ Blog/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 
@@ -23,9 +26,36 @@ def contact(request):
     return render(request, 'Blog/contact.html')
 
 
-def login(request):
+def signin(request):
+    if request.user.is_authenticated == True:
+        return redirect('blog:home')
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        print(user, )
+        if user is not None:
+            login(request, user)
+            return redirect('blog:home')
     return render(request, 'Blog/login.html')
 
 
 def register(request):
+    if request.user.is_authenticated:
+        return redirect('blog:home')
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        user = User.objects.create(username=username, email=email, password=password1)
+        login(request, user)
+        return redirect('blog:home')
     return render(request, 'Blog/signup.html')
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('blog:home')
