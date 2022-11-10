161e1f4fc773e399f856e21ae3455f4c205aa02e
Msa2001 <msa2001b21gmail.com>
2022-11-10 13:18:36 +0330

    templates footer added

--- context_processors/context_processors.py
+++ context_processors/context_processors.py
@@ -2,4 +2,8 @@ from Blog.models import Article, Category
 
 def Breakingnews(request):
     article = Article.objects.filter(category=4).order_by('-updated')
-    return {'article': article}
+    return {'articles': article}
+
+def more_news(request):
+    news = Article.objects.all().order_by('?')
+    return {'news': news}
