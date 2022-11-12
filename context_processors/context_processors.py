207974ff39ee9d01e3c796e6516e852c8428b53f
MSA2001 <msa2001b21@gmail.com>
2022-11-12 15:52:04 +0330

    breaking news bug fixed

--- context_processors/context_processors.py
+++ context_processors/context_processors.py
@@ -1,8 +1,10 @@
 from Blog.models import Article, Category
 
+
 def Breakingnews(request):
-    article = Article.objects.filter(category=4).order_by('-updated')
-    return {'articles': article}
+    breaking_news = Article.objects.filter(category=4).order_by('-updated')
+    return {'breaking_news': breaking_news}
+
 
 def more_news(request):
     news = Article.objects.all().order_by('?')
