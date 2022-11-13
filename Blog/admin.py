49d0847f53da5645255d1134eebcd907f70797eb
MSA2001 <msa2001b21@gmail.com>
2022-11-13 17:12:47 +0330

    comments model

--- Blog/admin.py
+++ Blog/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Article, Category, Message, Newsletter, Info
+from .models import Article, Category, Message, Newsletter, Info, Comment
 # Register your models here.
 admin.site.register(Article)
 admin.site.register(Category)
 admin.site.register(Message)
 admin.site.register(Newsletter)
 admin.site.register(Info)
+admin.site.regisster(Comment)
